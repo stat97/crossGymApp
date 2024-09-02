1adda93d707a1ce16c33460499b84f46c2fd240f
Adri <ortizdev@proton.me>
2024-09-02 18:45:21 +0200

    changing route

--- frontend/src/pages/CheckCode/CheckCode.jsx
+++ frontend/src/pages/CheckCode/CheckCode.jsx
@@ -93,7 +93,7 @@ export const CheckCode = () => {
     if (!localStorage.getItem('user')) {
       useAutoLogin(allUser, login);
     } else {
-      return <Navigate to="/dashboard" />;
+      return <Navigate to="/FormProfile" />;
     }
     
   }
